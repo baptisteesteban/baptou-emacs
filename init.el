a3f36503bc1f542e342abff0d09e703e282d0b8e
Baptiste Esteban <baptiste.esteban@epita.fr>
2020-02-07 19:14:12 +0100

    [INIT]: First commit

---
+++ init.el
@@ -0,0 +1,38 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; ===========================================
+;;   PUT THE PACKAGE WE WANT TO INSTALL HERE
+;; ===========================================
+
+(use-package cmake-mode)
+(use-package zenburn-theme)
+
+;; Load theme zenburn
+(load-theme 'zenburn t)
+
+;; neotree --sidebar for project file navigation
+(use-package neotree
+  :config (global-set-key "\C-x\ d" 'neotree-toggle))
+(neotree-refresh)
+(neotree-toggle)
